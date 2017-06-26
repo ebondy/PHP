3c7e9c0b6d2afb57d4f4613d66b871278c08cc77
Eric Bondy <bondy7_7@hotmail.com>
2017-06-26 18:05:42 -0400

    Add files via upload

---
+++ test1.php
@@ -0,0 +1,29 @@
+<?php
+
+//comment and variable practice
+$username = "Eric Bondy";
+echo $username;
+
+echo "<br>";
+
+$current_user = $username;
+echo $current_user;
+
+echo "<br>";
+
+//Array practice
+$array1 = array('Eric', 'Jose', 'Frank');
+
+echo $array1[2];
+
+echo "<br>";
+
+
+//Two dimentional array practice
+$array2 = array(array('jules', 'titi', 'lucian'),
+                array('bella', 'ricky', 'Trent'));
+
+echo $array2[1][1]; //first value is row second value is column
+
+
+?>
